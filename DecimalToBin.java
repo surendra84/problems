656a79f10eedc92d8656f0335d487df58037e878
Surendra <surendra84@gmail.com>
2022-11-05 17:52:36 -0700

    DecimalToBin - Bin To Dec and Dec to Bin
    Recursive program to convert the value Binary to Decimal and Decimal to Binary.

--- DecimalToBin.java
+++ DecimalToBin.java
@@ -1,19 +1,39 @@
+
 public class DecimalToBin {
     public static void main(String[] args) {
         System.out.println("5 in Binary: ");
-        printInBinFromDec(5);
+        printDecimalToBinary(5);
 
         System.out.println("\n7 in Binary: ");
-        printInBinFromDec(7);
+        printDecimalToBinary(7);
 
         System.out.println("\n16 in Binary: ");
-        printInBinFromDec(16);
+        printDecimalToBinary(16);
+        System.out.println("\nPrint Binary to Decimal");
+        System.out.println("Printing 101 in Decimal: "+printBinaryToDecimal(101, 0));
+        System.out.println("Printing 111 in Decimal: "+printBinaryToDecimal(111, 0));
+        System.out.println("Printing 1000 in Decimal: "+printBinaryToDecimal(10000, 0));
     }
 
-    public static void printInBinFromDec(int num) {
+    /**
+     * Recursive program to print Decimal to Binary
+     * @param num
+     */
+    public static void printDecimalToBinary(int num) {
         if(num==0) return;
-        printInBinFromDec(num/2);
+        printDecimalToBinary(num/2);
         System.out.print(num%2);
+    }
+
+    /**
+     * Recursive program to print Binary to Decimal
+     * @param num
+     * @param index
+     * @return
+     */
+    public static int printBinaryToDecimal(int num, int index) {
+        if(num==0) return 0;
+        return (int) (num%10*Math.pow(2, index))+printBinaryToDecimal(num/10, index+1);
     }
 
 }
